0f55e454ccc4299f120c745166e80db1859e65a2
Vanessa Beattie <vanessabeattie@Vanessas-MacBook-Pro.local>
2023-08-26 17:42:57 -0500

    LAB Complete

--- server.js
+++ server.js
@@ -1,28 +1,48 @@
-const express = require('express');
+// const express = require('express');
 
-const app = express();
-const PORT = 3000;
+// const app = express();
+// const PORT = 3000;
 
 //data
 const answer = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
 
-//2. & 3. routes + params
-app.get('/greeting/:name', (req, res) => {
-    res.send(`Hello, ` +  req.params.name)
+const express = require('express')
+
+const app = express()
+const PORT = 3000
+
+//Routes
+
+//Greeting
+app.get('/greeting', (req, res) => {
+    res.send('Hello, Stranger!')
 })
 
-//calculate tip
-app.get('/tip/:total/:tipPercentage', (req, res) => {
-    res.send('Your tip will be: ' + (req.params.total * (req.params.tipPercentage/100)))
-}
-)
+app.get('/greeting/:name', (req, res) => {
+    const name = req.params.name
+    res.send(`Hello, ${name}!`)
+    // res.send(`Hello, ` + req.params.name + `!`)
+})
 
-//Magic 8 ball
-app. get('/magic/:question/:answer', (req, res) => {
-    res.send(req.params.question + '?' + `<h1>` + answer[req.params.answer] + `</h1>` )
+
+//Calculator
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    const total = req.params.total
+    const tipPercentage = req.params.tipPercentage
+
+    res.send(`Your tip is: $ ${total * tipPercentage / 100}`)
+
+    // another way
+    // res.send(`Your tip is: ` + req.params.total * req.params.tipPercentage / 100)
+})
+
+//Basic 8 ball
+app.get('/magic/:question', (req, res) => {
+    const question = req.params.question
+    res.send(`${question}? <h1>${answer[Math.floor(Math.random() * (answer.length - 1))]}</h1>`)
 })
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+console.log(`Server running on port ${PORT}`);
 })
